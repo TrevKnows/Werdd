274b6549a94138764b27de06560673f36ac592a0
TrevKnows <Trevor.Beaton11@gmail.com>
2022-03-17 19:39:43 -0400

    Removed unnecessary comments code

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -83,33 +83,13 @@ class ViewController: UIViewController {
         return stackView
     }()
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         setupUI()
         print("View Loaded")
-//        view.addSubview(helloLabel)
-//        view.addSubview(submitButton)
-//
-//
-//        NSLayoutConstraint.activate([
-//            helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-//            helloLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-//            helloLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-//            helloLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
-//
-//            /// Submit Button Contraints
-//            submitButton.topAnchor.constraint(equalTo: helloLabel.bottomAnchor, constant: 20),
-//            submitButton.leadingAnchor.constraint(equalTo: helloLabel                                                                                .leadingAnchor, constant: 40),
-//            submitButton.trailingAnchor.constraint(equalTo: helloLabel                                                                          .trailingAnchor, constant: -40),
-//            submitButton.heightAnchor.constraint(equalToConstant: 200)
-//
-//        ])
-        
+
     }
 
     func setupUI(){
@@ -117,26 +97,6 @@ class ViewController: UIViewController {
         setupLabels()
         setUpSubmitButton()
     }
-    
-
-    
-//    func setupBlueView(){
-//        view.addSubview(blueView)
-//
-//        NSLayoutConstraint.activate([
-//            blueView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-//            blueView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            blueView.heightAnchor.constraint(equalToConstant: 200)
-//        ])
-//
-//        blueView.addSubview(helloLabel)
-//
-//        NSLayoutConstraint.activate([
-//            helloLabel.centerXAnchor.constraint(equalTo: blueView.centerXAnchor),
-//            helloLabel.centerYAnchor.constraint(equalTo: blueView.centerYAnchor),
-//        ])
-//    }
     
     func setUpSubmitButton() {
         
@@ -164,30 +124,6 @@ class ViewController: UIViewController {
         stackView.addArrangedSubview(tealLabel)
         stackView.addArrangedSubview(greenLabel)
         
-        
-        //        view.addSubview(orangeLabel)
-//        view.addSubview(tealLabel)
-//        view.addSubview(greenLabel)
-//
-//
-//
-//        orangeLabel.backgroundColor = .yellow
-//        tealLabel.backgroundColor = .yellow
-//        greenLabel.backgroundColor = .yellow
-//
-//        NSLayoutConstraint.activate([
-//            orangeLabel.topAnchor.constraint(equalTo: blueView.bottomAnchor, constant: 40),
-//            orangeLabel.leadingAnchor.constraint(equalTo: blueView.leadingAnchor),
-//            orangeLabel.trailingAnchor.constraint(equalTo: blueView.trailingAnchor),
-//
-//            tealLabel.topAnchor.constraint(equalTo: orangeLabel.bottomAnchor, constant: 20),
-//            tealLabel.leadingAnchor.constraint(equalTo: blueView.leadingAnchor),
-//            tealLabel.trailingAnchor.constraint(equalTo: blueView.trailingAnchor),
-//
-//            greenLabel.topAnchor.constraint(equalTo: tealLabel.bottomAnchor, constant: 20),
-//            greenLabel.leadingAnchor.constraint(equalTo: blueView.leadingAnchor),
-//            greenLabel.trailingAnchor.constraint(equalTo: blueView.trailingAnchor)
-//        ])
     }
     
     private func setupBlueView() {
@@ -206,9 +142,6 @@ class ViewController: UIViewController {
             helloLabel.centerXAnchor.constraint(equalTo: blueView.centerXAnchor),
             helloLabel.centerYAnchor.constraint(equalTo: blueView.centerYAnchor),
         ])
-
-
     }
-    
 }
 
